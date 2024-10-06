d6b6c030b4950d7845ee03a42115bbfb8a81c2dd
CrimsonFrost77 <grayishbluegamer@gmail.com>
2024-10-06 19:11:25 +0600

    seperated navbar into its own file

--- forms.php
+++ forms.php
@@ -10,65 +10,7 @@
   </head>
 
   <body>
-    <div class = "navbar">
-      <div class="imageSection">
-        <img class="logoAtTop" src="Assets/kpLogo.png" alt="Kaliakair Pauroshova Logo">
-            <!--this is a test comment-->
-      </div>
-      <div class="navbarSection">
-        <div class="dropdown">
-          <a href="index.php">
-            <button class="drpBtn">Home</button>
-          </a>
-        </div>
-        <div class="dropdown">
-          <button class="drpBtn">About</button>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <button class="drpBtn">Employees</button>
-          <!--<div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>-->
-        </div>
-        <div class="dropdown">
-          <button class="drpBtn">Projects</button>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <button class="drpBtn">Budgets</button>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <a href="forms.php">
-            <button class="drpBtn">Forms</button>
-          </a>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <button class="drpBtn">Contacts</button>
-        </div>
-      </div>
-      
-    </div>
+    <?php include 'navbar.php'; ?>
     
     <header class = "formsPageHeading">
       <h1>Forms</h1>
---
+++ navbar.php
@@ -0,0 +1,60 @@
+<!-- navbar.php -->
+<div class = "navbar">
+  <div class="imageSection">
+    <img class="logoAtTop" src="Assets/kpLogo.png" alt="Kaliakair Pauroshova Logo">
+        <!--this is a test comment-->
+  </div>
+  <div class="navbarSection">
+    <div class="dropdown">
+      <a href="index.php">
+        <button class="drpBtn">Home</button>
+      </a>
+    </div>
+    <div class="dropdown">
+      <button class="drpBtn">About</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="drpBtn">Employees</button>
+      <!--<div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>-->
+    </div>
+    <div class="dropdown">
+      <button class="drpBtn">Projects</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="drpBtn">Budgets</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <a href="forms.php">
+        <button class="drpBtn">Forms</button>
+      </a>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="drpBtn">Contacts</button>
+    </div>
+  </div>
+  
+</div>

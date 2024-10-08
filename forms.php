62794f3c16b8816b27b6070a319c1506609b8074
CrimsonFrost77 <grayishbluegamer@gmail.com>
2024-10-08 18:38:06 +0600

    slight changes

--- forms.php
+++ forms.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title>Kaliakair Pourashava</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="general.css">
     <link rel="stylesheet" href="navBar.css">
--- navbar.php
+++ navbar.php
@@ -2,7 +2,6 @@
 <div class = "navbar">
   <div class="imageSection">
     <img class="logoAtTop" src="Assets/kpLogo.png" alt="Kaliakair Pauroshova Logo">
-        <!--this is a test comment-->
   </div>
   <div class="navbarSection">
     <div class="dropdown">
@@ -12,11 +11,6 @@
     </div>
     <div class="dropdown">
       <button class="drpBtn">About</button>
-      <div class="dropdown-content">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        <a href="#">Link 3</a>
-      </div>
     </div>
     <div class="dropdown">
       <button class="drpBtn">Employees</button>
